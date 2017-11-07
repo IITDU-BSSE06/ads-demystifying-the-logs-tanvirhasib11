070e022b37683ed7a4a35b8e6a39550b652d3f12
tanvirhasib11 <bsse0639@iit.du.ac.bd>
2017-11-07 18:40:47 +0600

    a2 reducer.py

--- assignment2/reducer.py
+++ assignment2/reducer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
-
 import sys
+
 number = 0
 for line in sys.stdin:
-    data_mapped = line.strip().split("\t")
-    if len(data_mapped) != 2:
+    mapping_data = line.strip().split("\t")
+    if len(mapping_data) != 2:
         continue
-    path, other = data_mapped
+    path, other = mapping_data
     if '/assets/js/the-associates.js' in path:
      number = number + 1
 print number
