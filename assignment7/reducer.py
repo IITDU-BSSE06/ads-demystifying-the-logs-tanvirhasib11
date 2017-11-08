3189949618f72d0c6f3a8786f67af22a16fb2755
tanvirhasib11 <bsse0639@iit.du.ac.bd>
2017-11-08 13:03:53 +0600

    Update reducer.py

--- assignment7/reducer.py
+++ assignment7/reducer.py
@@ -1,13 +1,1 @@
-#!/usr/bin/python
-import operator
-import sys
-d = dict()
-for line in sys.stdin:
-    year = line.strip()
-    if year in d:
-      d[year] +=1
-    else: 
-      d[year] = 1
 
-for keys in d:
-print (keys,d[keys])
