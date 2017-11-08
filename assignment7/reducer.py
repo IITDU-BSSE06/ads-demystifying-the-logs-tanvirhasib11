8fccb79cf0d57f167f9c5b1a6e015abfebf2bbb8
tanvirhasib11 <bsse0639@iit.du.ac.bd>
2017-11-08 13:05:07 +0600

    a7 reducer.py

--- assignment7/reducer.py
+++ assignment7/reducer.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python
-import urlparse
 import sys
 
+maximum=0
+total_sales = 0
+Key1 = None
+name_path1=""
+name_file=""
+
 for line in sys.stdin:
-	data_set = line.strip().split(" ")
-	if len(data_set) == 10:
-		dt0, dt1 ,dt2 ,dt3 ,dt4 ,dt5 ,dt6 ,dt7 ,dt8 ,dt9= data_set
-		print "{0}\t{1}".format(dt6,dt6)
+    mapping_data = line.strip().split("\t")
+    if len(mapping_data) != 2:
+        continue
+
+    Key, fullPath = mapping_data
+
+    if Key1 and Key1 != Key:
+        Key1 = Key;
+        if total_sales > maximum:
+            maximum=total_sales
+            name_file=Key
+            name_path1=fullPath
+        total_sales = 0
+
+    Key1 = Key
+    total_sales += 1
+
+print str(maximum)
 
