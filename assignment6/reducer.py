3026b87193089652e50e6c3636c2c2f09420b7c1
tanvirhasib11 <bsse0639@iit.du.ac.bd>
2017-11-08 12:51:31 +0600

    a6 reducer.py

---
+++ assignment6/reducer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import sys
+num = 0
+num1 = 0
+num2 = 0
+
+for line in sys.stdin:
+	ip = line.strip()
+	if ip == "10.223.157.186"
+		num = num + 1
+	else if ip == "10.50.199.54"
+		num1 = num1 + 1
+	else if ip == "10.217.151.145"
+		num2 = num2 + 1
+print "2009" num
+print "2010" num1
+print "2011" num2
