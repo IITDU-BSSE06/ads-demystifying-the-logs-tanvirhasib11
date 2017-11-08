d1e29e4aea0b2be24a3c62935d5268f16c23cdc7
tanvirhasib11 <bsse0639@iit.du.ac.bd>
2017-11-08 13:04:32 +0600

    a7 mapper.py

--- assignment7/reducer.py
+++ assignment7/reducer.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python
+import urlparse
+import sys
+
+for line in sys.stdin:
+	data_set = line.strip().split(" ")
+	if len(data_set) == 10:
+		dt0, dt1 ,dt2 ,dt3 ,dt4 ,dt5 ,dt6 ,dt7 ,dt8 ,dt9= data_set
+		print "{0}\t{1}".format(dt6,dt6)
 
